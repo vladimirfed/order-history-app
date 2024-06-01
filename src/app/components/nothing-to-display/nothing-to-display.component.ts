7726a55d58790dd2421bd252edd6a0cf0a918f93
Vladimir Fedarenka <vfedorenko074@gmail.com>
2024-06-01 09:59:12 +0200

    Update responsive for filter fields

--- src/app/components/nothing-to-display/nothing-to-display.component.ts
+++ src/app/components/nothing-to-display/nothing-to-display.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { MatTooltip } from '@angular/material/tooltip';
 
 @Component({
   selector: 'app-nothing-to-display',
   standalone: true,
-  imports: [],
+  imports: [MatIconModule, MatTooltip],
   templateUrl: './nothing-to-display.component.html',
   styleUrl: './nothing-to-display.component.less'
 })
